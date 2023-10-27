e685d5945b3e09529f8e6384e72ddf1982a10e1e
victor26sp <82735535+victor26sp@users.noreply.github.com>
2023-10-27 09:23:44 -0300

    carrinho
    envio pelo whatsaapp

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let favoriteFilter = false;
 let skuToProductMap = {}; // Mapeamento SKU para produtos
 let cartItems = [];
 
+const whatsappNumber = "SEU_NUMERO_DE_WHATSAPP"; // Substitua pelo seu número de WhatsApp
+
 function toggleFavorite(btn) {
     btn.classList.toggle('btn-danger');
     btn.classList.toggle('btn-primary');
@@ -56,6 +58,7 @@ function createSizeGrid(product) {
         quantityInput.min = 0;
         quantityInput.max = sizeObj.stock; // Defina o estoque máximo disponível
         quantityInput.value = '0';
+        quantityInput.dataset.productSku = product.sku;
 
         const inputGroupText = document.createElement('span');
         inputGroupText.classList.add('input-group-text');
@@ -108,7 +111,7 @@ function renderCatalog(products) {
             if (
                 !product.ref.toLowerCase().includes(searchInput) &&
                 !product.description.toLowerCase().includes(searchInput) &&
-                !product.group.toLowerCase().includes(searchInput)
+                !product.category.toLowerCase().includes(searchInput)
             ) {
                 return false; // Não corresponde à pesquisa
             }
@@ -139,6 +142,10 @@ function renderCatalog(products) {
             <p class="card-text">Tamanhos Disponíveis: ${sizes}</p>
         </div>
     </div>
+    <div class="card-footer">
+        <button class="btn btn-primary add-to-cart" data-index="${index}">Adicionar ao Carrinho</button>
+        <button class="btn btn-success send-whatsapp" data-index="${index}">Enviar via WhatsApp</button>
+    </div>
     `;
     
         col.appendChild(card);
@@ -153,6 +160,14 @@ function renderCatalog(products) {
     addToCartButtons.forEach(button => {
         button.addEventListener('click', () => {
             addToCart(button);
+        });
+    });
+
+    // Adicione event listener para botões "Enviar via WhatsApp"
+    const sendWhatsappButtons = document.querySelectorAll('.send-whatsapp');
+    sendWhatsappButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            sendWhatsApp(button);
         });
     });
 }
@@ -255,166 +270,39 @@ function showVideo(product) {
     }
 }
 
-importCSV('products.csv');
+function sendWhatsApp(button) {
+    const productIndex = parseInt(button.getAttribute('data-index'));
+    const product = products[productIndex];
+    const selectedSize = getSelectedSize(product);
 
-function printCatalog() {
-    const favoriteProducts = getFavoriteProducts();
-
-    if (favoriteProducts.length === 0) {
-        alert('Nenhum item favoritado para imprimir.');
-        return;
+    if (selectedSize) {
+        const ref = product.ref;
+        const description = product.description;
+        sendWhatsAppMessage(ref, description, selectedSize);
+    } else {
+        alert('Selecione um tamanho antes de enviar via WhatsApp.');
     }
-
-    const printableContent = generatePrintableHTML(favoriteProducts);
-
-    const printWindow = window.open('', '_blank');
-    printWindow.document.open();
-    printWindow.document.write(printableContent);
-    printWindow.document.close();
-
-    printWindow.onload = function () {
-        printWindow.print();
-        setTimeout(function () {
-            printWindow.close();
-        }, 1000);
-    };
 }
 
-function getFavoriteProducts() {
-    const favoriteProducts = products.filter(product => product.isFavorite);
-    return favoriteProducts;
+function getSelectedSize(product) {
+    const sizeInputs = document.querySelectorAll(`input[data-product-sku="${product.sku}"]`);
+    for (const sizeInput of sizeInputs) {
+        if (sizeInput.value > 0) {
+            return sizeInput.value;
+        }
+    }
+    return null;
 }
 
-function generatePrintableHTML(products) {
-    const itemsPerPage = 1; // Cada item em uma página A4
-
-    const printableContent = products
-        .map((product, index) => {
-            const sizes = product.sizes.map(sizeObj => sizeObj.size).join(', ');
-
-            return `
-            <div class="printable-page">
-                <div class="printable-card">
-                    <h2 class="printable-title">${product.description}</h2>
-                    <p class="printable-text">Ref: ${product.ref}</p>
-                    <p class="printable-text">Cor: ${product.color}</p>
-                    <p class="printable-text">${product.category}</p>
-                    <p class="printable-text">${product.composition}</p>
-                    <div class="sizes-section">
-                        <p class="printable-text">Tamanhos Disponíveis: ${sizes}</p>
-                    </div>
-                    <img src="./imagens/${product.image}" class="printable-image" alt="${product.description}">
-                </div>
-            </div>
-        `;
-        })
-        .join('');
-
-    const printableHTML = `
-        <html>
-        <head>
-            <title>Catálogo Favoritado para Impressão</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                }
-                .printable-card {
-                    padding: 20px;
-                    border: 1px solid #ccc;
-                    margin: 20px 0;
-                    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
-                    page-break-inside: avoid;
-                    page-break-before: always; /* Iniciar nova página para cada item */
-                }
-                .printable-title {
-                    font-size: 1.5rem;
-                    margin-bottom: 10px;
-                }
-                .printable-text {
-                    font-size: 1rem;
-                    color: #555;
-                }
-                .printable-image {
-                    max-width: 70%;
-                    height: auto;
-                    margin-top: 10px;
-
-                }
-                .sizes-section {
-                    margin-top: 10px;
-                }
-                .size-grid {
-                    border-collapse: collapse;
-                    width: 100%;
-                }
-                .size-grid th,
-                .size-grid td {
-                    border: 1px solid #ddd;
-                    padding: 8px;
-                    text-align: center;
-                }
-                .size-grid th {
-                    background-color: #f2f2f2;
-                }
-                .quantity-control {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                }
-                .quantity-control button {
-                    background-color: #007bff;
-                    color: #fff;
-                    border: none;
-                    padding: 5px 10px;
-                    cursor: pointer;
-                }
-            </style>
-        </head>
-        <body>
-            ${printableContent}
-        </body>
-        </html>
-    `;
-
-    return printableHTML;
+function generateWhatsAppMessage(ref, description, size) {
+    const message = `Olá, tenho interesse no seguinte produto:\n\n`;
+    return `${message}Ref: ${ref}\nDescrição: ${description}\nTamanho: ${size}\n\n`;
 }
 
-function favoritarTodos() {
-    products.forEach(product => {
-        product.isFavorite = true;
-    });
-    renderCatalog(products);
+function sendWhatsAppMessage(ref, description, size) {
+    const message = generateWhatsAppMessage(ref, description, size);
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappLink, '_blank');
 }
 
-function desfavoritarTodos() {
-    products.forEach(product => {
-        product.isFavorite = false;
-    });
-    renderCatalog(products);
-}
-
-document.getElementById('favoritarTodos').addEventListener('click', function () {
-    favoritarTodos();
-});
-
-document.getElementById('desfavoritarTodos').addEventListener('click', function () {
-    desfavoritarTodos();
-});
-
-document.getElementById('printButton').addEventListener('click', printCatalog);
-
-document.getElementById('categoryFilter').addEventListener('change', function () {
-    categoryFilter = this.value;
-    renderCatalog(products);
-});
-
-document.getElementById('favoriteFilterInput').addEventListener('change', function () {
-    favoriteFilter = this.checked;
-    renderCatalog(products);
-});
-
-document.getElementById('clearFiltersButton').addEventListener('click', function () {
-    categoryFilter = '';
-    favoriteFilter = false;
-    renderCatalog(products);
-});
+importCSV('products.csv');
